084d519af43f597c977f7a0313113c5e71ab8d62
Dzianis_Latushka <dzianis_latushka@epam.com>
2020-03-22 16:46:18 +0300

    Initial commit

---
+++ app/src/main/java/by/itacademy/android/m_ad2_13_20/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.itacademy.android.m_ad2_13_20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
